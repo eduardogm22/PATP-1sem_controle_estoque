fbfe6dcb02216b2ffb47bd81b5f8d8729ab20a74
eduardogm22 <edu_grosselli@hotmail.com>
2024-06-11 21:53:40 -0300

    implementado um esboço de CRUD

--- criar_bd.py
+++ criar_bd.py
@@ -4,4 +4,4 @@ con = lite.connect("dados.db")
 
 with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE produtos(codigo INTEGER PRIMARY KEY AUTOINCREMENT, desc TEXT, medida TEXT, fornecedor TEXT, fab TEXT, val TEXT)")
+    cur.execute("CREATE TABLE produtos(id INTEGER PRIMARY KEY AUTOINCREMENT, desc TEXT, medida TEXT, fornecedor TEXT, fab TEXT, val TEXT)")
--- crud.py
+++ crud.py
@@ -9,7 +9,24 @@ with con:
     cur = con.cursor()
     query = "INSERT INTO produtos(desc, medida, fornecedor, fab, val) VALUES(?, ?, ?, ?, ?)"
     cur.execute(query, dados)
-"""
+
+#crud
+
+
+atualizar_dados = ["Becker", "5ML", "Descarpack", "10/08/2023", "Indefinido", 1 ]
+#atualizar produtos
+with con:
+    cur = con.cursor()
+    query = "UPDATE produtos SET desc=?, medida=?, fornecedor=?, fab=?, val=? WHERE id=? "
+    cur.execute(query,atualizar_dados)"""
+    
+deletar_dados = str(4)  
+#remover produtos
+with con:
+    cur = con.cursor()
+    query = "DELETE FROM produtos WHERE id=?"
+    cur.execute(query,deletar_dados)
+
 ver_dados = []
 
 #visualizar produtos

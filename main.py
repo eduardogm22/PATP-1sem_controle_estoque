a2e0b47322b4f8c79e2fb9db6d160d7723fb0e51
eduardogm22 <edu_grosselli@hotmail.com>
2024-06-20 22:44:17 -0300

    programada funçao de abrir a janela de cadastro ao clicar no botao adicionar

--- main.py
+++ main.py
@@ -3,19 +3,33 @@ from PyQt5.Qt import Qt
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtPrintSupport import *
 import os, sys
-
 from visualizar import Ui_Visualizar
-class visualizar(QDialog):
+from adicionar import Ui_adicionar
+class adicionar(QtWidgets.QMainWindow):
+    def __init__(self,*args,**argvs):
+        super(adicionar,self).__init__(*args,**argvs)
+        self.ui = Ui_adicionar()
+        self.ui.setupUi(self)
+class visualizar(QtWidgets.QMainWindow):
     def __init__(self,*args,**argvs):
         super(visualizar,self).__init__(*args,**argvs)
         self.ui = Ui_Visualizar()
         self.ui.setupUi(self)
+        self.ui.btn_add.clicked.connect(self.add)
+        
+    def add(self):
+        self.add_prod = adicionar()
+        self.add_prod.show()
+            
+            
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = visualizar()
+    MainWindow.show()
+    sys.exit(app.exec_())
 
-app = QApplication(sys.argv)
-if (QDialog.Accepted == True):
-    window = visualizar()
-    window.show()
-sys.exit(app.exec_())
+    
--- visualizar.py
+++ visualizar.py
@@ -1,5 +1,6 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class Ui_Visualizar(object):
